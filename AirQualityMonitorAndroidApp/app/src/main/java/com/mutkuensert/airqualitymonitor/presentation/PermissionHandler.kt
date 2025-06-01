f5a0720740f6c9dd5f804ae9ef275dc62a976bb5
M. Utku Ensert <mutkuensert@hotmail.com>
2025-06-01 16:20:50 +0300

    fix: remove shouldShowRequestPermissionRationale
    It prevents permission request

--- AirQualityMonitorAndroidApp/app/src/main/java/com/mutkuensert/airqualitymonitor/presentation/PermissionHandler.kt
+++ AirQualityMonitorAndroidApp/app/src/main/java/com/mutkuensert/airqualitymonitor/presentation/PermissionHandler.kt
@@ -4,16 +4,13 @@ import android.Manifest
 import android.app.Activity
 import android.os.Build
 import androidx.core.app.ActivityCompat.requestPermissions
-import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 
 object PermissionHandler {
 
     fun requestNotificationPermission(activity: Activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
             val permission = Manifest.permission.POST_NOTIFICATIONS
-            if (shouldShowRequestPermissionRationale(activity, permission)) {
-                requestPermissions(activity, arrayOf(permission), 0)
-            }
+            requestPermissions(activity, arrayOf(permission), 0)
         }
     }
 }
